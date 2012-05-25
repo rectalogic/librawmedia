0796aecafdfc0923f60f5009fa68d212cdf0e2ee
Andrew Wason <rectalogic@rectalogic.com>
2012-05-25 10:55:44 -0400

    Expose framerate from ruby Session.

--- ruby/rawmedia.rb
+++ ruby/rawmedia.rb
@@ -9,6 +9,7 @@ module RawMedia
     attr_reader :session
     attr_reader :width
     attr_reader :height
+    attr_reader :framerate
     def initialize(width, height, framerate=Rational(30))
       @session = Internal::RawMediaSession.new
       @session[:framerate_num] = framerate.numerator
@@ -18,6 +19,7 @@ module RawMedia
       Internal::check Internal::rawmedia_init_session(@session)
       @width = width
       @height = height
+      @framerate = framerate
     end
     def audio_framebuffer_size
       @session[:audio_framebuffer_size]
